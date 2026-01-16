e31a6740f0d115684fad054cdaa53ed9b1593cc9
Raimundo Neto <raimundoneto461@gmail.com>
2026-01-16 10:23:43 -0300

    Refactor Calculator class to use static methods
    Updated Calculator methods and PI to be static.

--- src/praticas/Calculator.java
+++ src/praticas/Calculator.java
@@ -1,12 +1,13 @@
 package util;
 
 public class Calculator {
-	public Double PI = 3.14159;
+	public static Double PI = 3.14159;
 	
-	public Double circunferencia(Double raio) {
+	public static Double circunferencia(Double raio) {
 		return 2.0 * PI * raio;
 	}
-	public Double volume(Double raio) {
+	public static Double volume(Double raio) {
 		return 4.0 * PI * raio * raio * raio/3;
 	}
 }
+
